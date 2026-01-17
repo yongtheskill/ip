f893985429185833f91c4821c379e2bf66c5ef8d
Yong Ee <yongee2003@gmail.com>
2026-01-18 03:55:41 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Six.java
@@ -0,0 +1,11 @@
+public class Six {
+    public static void main(String[] args) {
+        String intro = "____________________________________________________________\n" +
+                " Hello! I'm Six\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(intro);
+    }
+}
